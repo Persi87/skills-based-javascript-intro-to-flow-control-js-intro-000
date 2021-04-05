58cd81fd8c512a76986f235c291ad17f5c4752b1
effective-partial-f6ad <simon_black87@hotmail.co.uk>
2021-04-05 16:20:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,11 @@
 function basicTeenager(age) {
-  if(age > 12 && age < 20) {
+  if(age >= 13 && age <= 19) {
   return "You are a teenager!"
  }
 }
 
 function teenager(age) {
-  if (age > 12 && age < 20) {
-
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   } else {
     return "You are not a teenager"
@@ -15,7 +14,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age < 12 && age > 20){
+  if (age >= 13 && age <= 19){
     return "You are a teenager!"
   } else if (age <= 12){
       return "You are a kid"
